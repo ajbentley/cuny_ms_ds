979ad2e304aed065b35041f6c4f1cc06aebfe4b7
ajoshuabentley@yahoo.com <ajoshuabentley@yahoo.com>
2018-07-14 13:37:28 -0400

    SQL HW 1 update

--- sql_wk1_hw.sql
+++ sql_wk1_hw.sql
@@ -1,16 +1,25 @@
--- Q1. Which destination in the flights database is the furthest distance away, based on information in the flights table. 
-SELECT dest, MAX(distance) FROM flights;
 
--- A1: IAH
+-- Q1. Which destination in the flights database is the furthest distance away, based on information in the flights table. 
+SELECT origin, dest, MAX(distance) FROM flights;
+
+/*A1: The longest distance for a flight according to the data was 4,983 miles, though this was a flight from Newark to George Bush Intercontinental/Houstaon Airport (AIH), which
+just doesn't make sense since the two airports are only 1,627 miles away from each other. That being the case I revisited the code and came up with the below, JFK to Honolulu (with
+the same mileage, which made much more sense.*/
+
+SELECT origin, dest, distance FROM flights
+ORDER BY distance DESC
+LIMIT 1;
+
 
 -- Q2a. What are the different numbers of engines in the planes table? 
-SELECT DISTINCT(engine) FROM planes;
+SELECT COUNT(*) FROM planes;
 
 -- A2a. Turbo-fan, Turbo-et, Reciprocating, 4 Cycle, Turbo-shaft, and Turbo-prop
 
 -- Q2b. For each number of engines, which aircraft have the most number of seats?  
 SELECT engine, seats FROM planes
 GROUP BY engine
+# HAVING engine = "Turbo-prop" ## used this code to make sure I wasn't having a similar problem as in Q1.
 ORDER BY seats DESC;
 
 -- A2b. Turbo-jet
@@ -24,12 +33,14 @@ SELECT COUNT(*) FROM flights;
 SELECT carrier, COUNT(*) FROM flights
 GROUP BY carrier
 ;
+-- A4. Please run and see below (in other words please don't make me type this all out!)
 
 -- Q5. Show all of the airlines, ordered by number of flights in descending order. 
 SELECT carrier, COUNT(*) FROM flights
 GROUP BY carrier
 ORDER BY COUNT(*) DESC
 ;
+-- A5. Please see A4
 
 -- Q6. Show only the top 5 airlines, by number of flights, ordered by number of flights in descending order
 SELECT carrier, COUNT(*) FROM flights
